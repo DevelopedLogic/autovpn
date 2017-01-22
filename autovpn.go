3db276c725a1e1a101dbe989324fa08f3f11cc2e
Steve Phillips <steve@tryingtobeawesome.com>
2017-01-22 08:00:53 -0800

    Updated autovpn.go: Same great functionality, much less indentation-y taste

--- autovpn.go
+++ autovpn.go
@@ -27,60 +27,60 @@ func main() {
 
 	fmt.Printf("[autovpn] getting server list\n")
 	response, err := http.Get(URL)
-	if err != nil {
-		panic(err)
-	} else {
-		defer response.Body.Close()
-		csvFile, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			panic(err)
-		} else {
-			fmt.Printf("[autovpn] parsing response\n")
-			fmt.Printf("[autovpn] looking for %s\n", chosenCountry)
+	check(err)
 
-			for i, line := range strings.Split(string(csvFile), "\n") {
-				if i > 1 {
-					splits := strings.Split(line, ",")
-					if len(splits) < 15 {
-						break
-					}
+	defer response.Body.Close()
+	csvFile, err := ioutil.ReadAll(response.Body)
+	check(err)
 
-					country := splits[6]
-					conf, err := base64.StdEncoding.DecodeString(splits[14])
-					if err == nil && chosenCountry == country {
-						fmt.Printf("[autovpn] writing config file\n")
-						f, err := os.Create("/tmp/openvpnconf")
-						check(err)
+	fmt.Printf("[autovpn] parsing response\n")
+	fmt.Printf("[autovpn] looking for %s\n", chosenCountry)
 
-						_, err = f.Write(conf)
-						check(err)
+	for i, line := range strings.Split(string(csvFile), "\n") {
+		if i <= 1 {
+			continue
+		}
 
-						f.Close()
+		splits := strings.Split(line, ",")
+		if len(splits) < 15 {
+			break
+		}
 
-						fmt.Printf("[autovpn] running openvpn\n")
+		country := splits[6]
+		conf, err := base64.StdEncoding.DecodeString(splits[14])
+		if err != nil || chosenCountry != country {
+			continue
+		}
 
-						cmd := exec.Command("sudo", "openvpn", "/tmp/openvpnconf")
-						cmd.Stdout = os.Stdout
+		fmt.Printf("[autovpn] writing config file\n")
+		f, err := os.Create("/tmp/openvpnconf")
+		check(err)
 
-						c := make(chan os.Signal, 2)
-						signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-						go func() {
-							<-c
-							cmd.Process.Kill()
-						}()
+		_, err = f.Write(conf)
+		check(err)
 
-						cmd.Start()
-						cmd.Wait()
+		f.Close()
 
-						fmt.Printf("[autovpn] try another VPN? (y/n) ")
-						var input string
-						fmt.Scanln(&input)
-						if strings.ToLower(input) == "n" {
-							os.Exit(0)
-						}
-					}
-				}
-			}
+		fmt.Printf("[autovpn] running openvpn\n")
+
+		cmd := exec.Command("sudo", "openvpn", "/tmp/openvpnconf")
+		cmd.Stdout = os.Stdout
+
+		c := make(chan os.Signal, 2)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			<-c
+			cmd.Process.Kill()
+		}()
+
+		cmd.Start()
+		cmd.Wait()
+
+		fmt.Printf("[autovpn] try another VPN? (y/n) ")
+		var input string
+		fmt.Scanln(&input)
+		if strings.ToLower(input) == "n" {
+			os.Exit(0)
 		}
 	}
 }
