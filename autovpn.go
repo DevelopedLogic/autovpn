fd8764c151d35a17719e97c619bb14b74969b43c
drymonsoon <tirabassigiacomo@gmail.com>
2017-01-24 16:16:43 +0100

    used scanner instead of parsing all data and used ioutil.WriteFile

--- autovpn.go
+++ autovpn.go
@@ -1,86 +1,80 @@
 package main
 
 import (
-    "encoding/base64"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "os/exec"
-    "os/signal"
-    "strings"
-    "syscall"
+	"bufio"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-    chosenCountry := "US"
-    if len(os.Args) > 1 && len(os.Args[1]) == 2 {
-        chosenCountry = os.Args[1]
-    }
-    URL := "http://www.vpngate.net/api/iphone/"
+	chosenCountry := "US"
+	if len(os.Args) > 1 && len(os.Args[1]) == 2 {
+		chosenCountry = os.Args[1]
+	}
+	URL := "http://www.vpngate.net/api/iphone/"
 
-    fmt.Printf("[autovpn] getting server list\n")
-    response, err := http.Get(URL)
-    check(err)
+	fmt.Printf("[autovpn] getting server list\n")
+	response, err := http.Get(URL)
+	check(err)
 
-    defer response.Body.Close()
-    csvFile, err := ioutil.ReadAll(response.Body)
-    check(err)
+	defer response.Body.Close()
+	scanner := bufio.NewScanner(response.Body)
 
-    fmt.Printf("[autovpn] parsing response\n")
-    fmt.Printf("[autovpn] looking for %s\n", chosenCountry)
+	fmt.Printf("[autovpn] parsing response\n")
+	fmt.Printf("[autovpn] looking for %s\n", chosenCountry)
 
-    for i, line := range strings.Split(string(csvFile), "\n") {
-        if i <= 1 {
-            continue
-        }
+	first := false
+	for scanner.Scan() {
+		if first == false {
+			first = true
+			continue
+		}
+		splits := strings.Split(scanner.Text(), ",")
+		if len(splits) < 15 {
+			break
+		}
 
-        splits := strings.Split(line, ",")
-        if len(splits) < 15 {
-            break
-        }
+		country := splits[6]
+		conf, err := base64.StdEncoding.DecodeString(splits[14])
+		if err != nil || chosenCountry != country {
+			continue
+		}
 
-        country := splits[6]
-        conf, err := base64.StdEncoding.DecodeString(splits[14])
-        if err != nil || chosenCountry != country {
-            continue
-        }
+		fmt.Printf("[autovpn] writing config file\n")
+		ioutil.WriteFile("/tmp/openvpnconf", conf, 0664)
+		fmt.Printf("[autovpn] running openvpn\n")
 
-        fmt.Printf("[autovpn] writing config file\n")
-        f, err := os.Create("/tmp/openvpnconf")
-        check(err)
+		cmd := exec.Command("sudo", "openvpn", "/tmp/openvpnconf")
+		cmd.Stdout = os.Stdout
 
-        _, err = f.Write(conf)
-        check(err)
+		c := make(chan os.Signal, 2)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			<-c
+			cmd.Process.Kill()
+		}()
 
-        f.Close()
+		cmd.Start()
+		cmd.Wait()
 
-        fmt.Printf("[autovpn] running openvpn\n")
-
-        cmd := exec.Command("sudo", "openvpn", "/tmp/openvpnconf")
-        cmd.Stdout = os.Stdout
-
-        c := make(chan os.Signal, 2)
-        signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-        go func() {
-            <-c
-            cmd.Process.Kill()
-        }()
-
-        cmd.Start()
-        cmd.Wait()
-
-        fmt.Printf("[autovpn] try another VPN? (y/n) ")
-        var input string
-        fmt.Scanln(&input)
-        if strings.ToLower(input) == "n" {
-            os.Exit(0)
-        }
-    }
+		fmt.Printf("[autovpn] try another VPN? (y/n) ")
+		var input string
+		fmt.Scanln(&input)
+		if strings.ToLower(input) == "n" {
+			os.Exit(0)
+		}
+	}
 }
